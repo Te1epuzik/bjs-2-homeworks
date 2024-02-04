2d203b92deab37b421fac563f42b98ef38fe1cc2
Neto test <test@neto.com>
2024-02-04 19:47:41 +0300

    HomeWork 1 tast 1 correction

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,7 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-	let Discriminant = Math.pow(b, 2) - 4 * a * c;
+	let Discriminant = b** - 4 * a * c;
 	if (Discriminant < 0) {
 		return arr;
 	} else if (Discriminant === 0) {
@@ -10,9 +10,10 @@ function solveEquation(a, b, c) {
 		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
 		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
 	}
-  
+
   return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
