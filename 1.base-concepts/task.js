c036cc33b89fda16821d2d986705a71eb899d9b8
Neto test <test@neto.com>
2024-02-04 19:59:27 +0300

    HomeWork 1 task 1 correction

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,7 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-	let d = b** - 4 * a * c;
+	let d = Math.pow(b, 2) - 4 * a * c;
 	if (d === 0) {
 		arr[0] = -b / (2 * a);
 	} else if (d > 0) {
