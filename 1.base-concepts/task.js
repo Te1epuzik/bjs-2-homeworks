a432d5599d0733af96049494daa77a5b3f7f432f
Neto test <test@neto.com>
2024-02-04 19:35:50 +0300

    HomeWork 1 task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [1];
+  let arr = [];
+	let Discriminant = Math.pow(b, 2) - 4 * a * c;
+	if (Discriminant < 0) {
+		return arr;
+	} else if (Discriminant === 0) {
+		arr[0] = -b / (2 * a);
+	} else if (Discriminant > 0) {
+		arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+		arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+	}
   
   return arr;
 }
