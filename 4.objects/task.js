fab23ee5d5eaaa21c5f7fff995470f213fdd1451
Neto test <test@neto.com>
2024-02-15 14:09:13 +0300

    HomeWork 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+	this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+	if ("marks" in this) {
+		this.marks = marks;
+	};
 }
 
 Student.prototype.getAverage = function () {
-  
+	let marksAvg = 0;
+	if (("marks" in this) && (this.marks instanceof Array) && (this.marks.length !== 0)) {
+		let marksSum = 0;
+		this.marks.forEach(mark => marksSum += mark);
+		marksAvg = marksSum / this.marks.length;
+	};
+	return marksAvg;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 }
+
