6ef8a72e31f8033e43bd39ce52f300f9c844fdc8
Neto test <test@neto.com>
2024-02-21 14:10:45 +0300

    HomeWork classes

---
+++ 5.classes/task.js
@@ -0,0 +1,100 @@
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+		this.type = null;
+	}
+
+	fix() {
+		let newState = this.state * 1.5;
+		if (newState < 0) {
+			this.state = 0;
+		} else if (newState > 100) {
+			this.state = 100;
+		} else {
+			this.state = newState;
+		}
+	}
+
+	set setState(state) {
+		if (state < 0) {
+			this.state = 0;
+		} else if (state > 100) {
+			this.state = 100;
+		} else {
+			this.state = state;
+		}
+	}
+
+	get getState() {
+		return this.state;
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "magazine";
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.author = author;
+		this.type = "book";
+	}
+}
+
+class NovelBook extends Book {
+	constructor(name, releaseDate, pagesCount, author) {
+		super(name, releaseDate, pagesCount, author);
+		this.type = "novel";
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(name, releaseDate, pagesCount, author) {
+		super(name, releaseDate, pagesCount, author);
+		this.type = "fantastic";
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(name, releaseDate, pagesCount, author) {
+		super(name, releaseDate, pagesCount, author);
+		this.type = "detective";
+	}
+}
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	}
+
+	findBookBy(type, value) {
+		if (!this.books.find(book => book[type] === value)) {
+			return null;
+		}
+		return this.books.find(book => book[type] === value);
+	}
+
+	giveBookByName(bookName) {
+		if (this.books.find(book => book.name === bookName)) {
+			let selectedBook = this.books.find(book => book.name === bookName);
+			this.books.splice(this.books.indexOf(selectedBook), 1);
+			return selectedBook;
+		} else { 
+			return null;
+		}
+	}
+}
