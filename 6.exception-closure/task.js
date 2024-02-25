52ac2b951ca619ecbff06c4781cd7ba985ba217e
Neto test <test@neto.com>
2024-02-25 14:41:53 +0300

    HomeWork extentions

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(productQuantity) {
+	if (isNaN(parseFloat(productQuantity))) {
+		throw new Error("Невалидное значение");
+	}
+	return Number.parseFloat(productQuantity);
+}
+
+function validateCount(productQuantity) {
+	try {
+		return parseCount(productQuantity);
+	} catch (invalidCount) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if (a + b < c || a + c < b || b + c < a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+		this.aSide = a;
+		this.bSide = b;
+		this.cSide = c;
+	}
+
+	get perimeter() {
+		return this.aSide + this.bSide + this.cSide;
+	}
+
+	get area() {
+		let p = (this.aSide + this.bSide + this.cSide) / 2;
+		return Math.sqrt(p * (p - this.aSide) * (p - this.bSide) * (p - this.cSide));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch(doesntExist) {
+		return {
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			},
+			
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			}
+		};
+	}
+}
