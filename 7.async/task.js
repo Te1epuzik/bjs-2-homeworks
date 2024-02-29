2c42a7085f0852076f0cde61b15d9c1f624a38fd
Neto test <test@neto.com>
2024-02-29 22:44:29 +0300

    HomeWork async

---
+++ 7.async/task.js
@@ -0,0 +1,50 @@
+class AlarmClock {
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId;
+	}
+
+	addClock(time, callback) {
+		if (typeof time === "undefined" && typeof callback === "undefined") {
+			throw new Error("Отсутсвуют обязательные аргументы");
+		}
+
+		for (let alarm of this.alarmCollection) {
+			if (alarm.time === time) {
+				console.warn("Уже присутствует звонок на это время");
+			}
+		}
+
+		this.alarmCollection.push({
+			callback,
+			time: time,
+			canCall: true,
+		});
+
+	}
+
+	removeClock(time) {
+		if (this.alarmCollection.length !== 0) {
+			this.alarmCollection = this.alarmCollection.filter(timer => timer.time === time);
+		}
+	}
+
+	getCurrentFormattedTime() {
+		return new Date().toLocaleTimeString().slice(0, -3);
+	}
+
+	start() {
+		if (typeof IntervalId === "undefined") {
+			return;
+		}
+			
+		this.intervalId = () => {
+			this.alarmCollection.forEach(timer => {
+				if (timer.canCall === true && timer.time === new Date().toLocaleTimeString().slice(0, -3)) {
+					timer.canCall = false;
+					timer.callback();
+				}
+			})
+		}
+	}
+}
